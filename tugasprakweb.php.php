9adbf4afe8fe2cb11bd2a46f9d4ca9f5c03e1ed6
wahidamuntaza <wahidamuntazasuhadi123@gamil.com>
2019-10-22 14:56:32 +0700

    tugas1

---
+++ tugasprakweb.php.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "heloo";
+$jari = 10;
+
+if ($jari<0) {
+    echo "error";
+} else {
+    echo $nama;
+    $hasil = 3.14*$jari*$jari;
+    echo "<br> hasilnya :".$hasil."<br>";
+}
+
+for($i=0;$i<10;$i++){
+    echo "hai <br>";
+}
+?>
